ff2633f5c47d8bff5a7809f15d9ee5c7b58688d0
Joran Bergfeld <joran.bergfeld@microsoft.com>
2025-01-10 08:23:48 +0100

    expands site

--- src/main/java/com/joranbergfeld/info_page/InfoController.java
+++ src/main/java/com/joranbergfeld/info_page/InfoController.java
@@ -3,6 +3,7 @@ package com.joranbergfeld.info_page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class InfoController {
 
@@ -11,4 +12,18 @@ public class InfoController {
         return "info";
     }
 
+    @GetMapping("/blog")
+    public String blogPage() {
+        return "blog";
+    }
+
+    @GetMapping("/portfolio")
+    public String portfolioPage() {
+        return "portfolio";
+    }
+
+    @GetMapping("/reach-out")
+    public String reachOutPage() {
+        return "reach-out";
+    }
 }
